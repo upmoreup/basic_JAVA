8a5e0e1be7d058e95ac8c93af5c6b86d5e302326
grabnpush <grabnpush@gmail.com>
2025-04-25 17:33:33 +0900

    add: 배열 돌며 Sting 합치기

--- src/array/Array3.java
+++ src/array/Array3.java
@@ -1,7 +1,29 @@
 package array;
 
+import java.util.Scanner;
+
 public class Array3 {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
 
+        int[] numbers;
+        numbers = new int[5];
+
+        System.out.println("5개의 정수를 입력하세요:");
+
+        StringBuilder totalString = new StringBuilder();
+        for (int i = 0; i < numbers.length; i++) {
+            int inputNumber = sc.nextInt();
+
+            if (i == numbers.length - 1) {
+                totalString.append(inputNumber);
+            } else {
+                totalString.append(inputNumber).append(", ");
+            }
+
+        }
+
+        System.out.println("출력");
+        System.out.println(totalString);
     }
 }
